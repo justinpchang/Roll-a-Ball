c1a256a3ee85dd02dec16c80bcde49351a82f0a8
Justin Chang <justin.p.chang@gmail.com>
2016-04-26 19:30:43 -0400

    Initial Commit
    Basic game as per the instructions in the documentation on the Unity
    website

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float thrust;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * thrust);
+
+		if (count == 12) {
+			DisplayWinText ();
+		}
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count ++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString ();
+	}
+
+	void DisplayWinText() {
+		winText.text = "YOU HAVE WON!!!";
+	}
+
+}
